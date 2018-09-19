99fbc961cf436c66861712150d04753e4aa30a40
Bai Yunfei <baiyunfeiii@foxmali.com>
2018-09-20 00:03:34 +0800

    implement add item to shopping cart in shopping cart item service

--- src/main/java/cn/yfbai/shopbackend/service/ShoppingCartItemService.java
+++ src/main/java/cn/yfbai/shopbackend/service/ShoppingCartItemService.java
@@ -1,11 +1,17 @@
 package cn.yfbai.shopbackend.service;
 
 import cn.yfbai.shopbackend.entity.ShoppingCartItem;
+import cn.yfbai.shopbackend.repository.ShoppingCartItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ShoppingCartItemService {
+
+    @Autowired
+    private ShoppingCartItemRepository shoppingCartItemRepository;
+
     public ShoppingCartItem addItemToCart(ShoppingCartItem shoppingCartItem) {
-        return null;
+        return shoppingCartItemRepository.save(shoppingCartItem);
     }
 }
