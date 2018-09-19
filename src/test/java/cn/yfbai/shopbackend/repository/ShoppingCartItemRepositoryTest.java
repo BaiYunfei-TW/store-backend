a0b6b4940be1f580623b7d59e6cdefec91990e32
Bai Yunfei <baiyunfeiii@foxmali.com>
2018-09-20 00:05:43 +0800

    implement add item in shopping cart item repository

--- src/test/java/cn/yfbai/shopbackend/repository/ShoppingCartItemRepositoryTest.java
+++ src/test/java/cn/yfbai/shopbackend/repository/ShoppingCartItemRepositoryTest.java
@@ -15,6 +15,7 @@ import java.util.List;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
@@ -38,6 +39,24 @@ public class ShoppingCartItemRepositoryTest {
         ShoppingCartItem shoppingCartItem = new ShoppingCartItem(product, userId, quantity);
 
         entityManager.persist(shoppingCartItem);
+
+        List<ShoppingCartItem> items = shoppingCartItemRepository.findByUserId(userId);
+        assertThat(items.size(), is(1));
+        assertThat(items.get(0).getProduct(), equalTo(product));
+        assertThat(items.get(0).getQuantity(), is(quantity));
+    }
+
+    @Test
+    public void should_return_shopping_cart_item_with_id_when_save_new_item() {
+        Integer userId = 1;
+        int quantity = 10;
+
+        Product product = new Product();
+        product.setId("020c823b-0753-4107-8216-13d38dde724c");
+        ShoppingCartItem shoppingCartItem = new ShoppingCartItem(product, userId, quantity);
+
+        shoppingCartItem = shoppingCartItemRepository.save(shoppingCartItem);
+        assertThat(shoppingCartItem.getId(), notNullValue());
 
         List<ShoppingCartItem> items = shoppingCartItemRepository.findByUserId(userId);
         assertThat(items.size(), is(1));
