4739fc83ea7b53ce3e1d9de5fd3c117741be6800
Bai Yunfei <baiyunfeiii@foxmali.com>
2018-09-24 20:02:11 +0800

    add contract test for 'add item to shopping cart'

--- src/test/java/cn/yfbai/shopbackend/BaseMvcTest.java
+++ src/test/java/cn/yfbai/shopbackend/BaseMvcTest.java
@@ -2,9 +2,11 @@ package cn.yfbai.shopbackend;
 
 import cn.yfbai.shopbackend.controller.ProductController;
 import cn.yfbai.shopbackend.controller.ProductControllerTest;
+import cn.yfbai.shopbackend.controller.ShoppingCartItemController;
 import cn.yfbai.shopbackend.entity.Product;
 import cn.yfbai.shopbackend.helpers.SyntaxSugar;
 import cn.yfbai.shopbackend.service.ProductService;
+import cn.yfbai.shopbackend.service.ShoppingCartItemService;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.Before;
 import org.junit.runner.RunWith;
@@ -18,15 +20,22 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(ProductController.class)
+@WebMvcTest({
+        ProductController.class,
+        ShoppingCartItemController.class
+})
 @ContextConfiguration(classes = {ShopBackendApplication.class})
 public abstract class BaseMvcTest {
 
     @MockBean
     private ProductService productService;
+
+    @MockBean
+    private ShoppingCartItemService shoppingCartItemService;
 
     @Autowired
     private MockMvc mockMvc;
@@ -36,6 +45,7 @@ public abstract class BaseMvcTest {
         RestAssuredMockMvc.mockMvc(mockMvc);
 
         given(productService.getProducts()).willReturn(SyntaxSugar.createProductList());
+        given(shoppingCartItemService.addItemToCart(any())).willReturn(SyntaxSugar.createShoppingCartItem());
     }
 
 }
