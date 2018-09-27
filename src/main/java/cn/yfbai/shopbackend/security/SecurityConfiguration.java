7b4cad5d9bdc02a15ee924893d31fa8cb648b822
Bai Yunfei <baiyunfeiii@foxmali.com>
2018-09-27 14:07:45 +0800

    add security and filter all request

---
+++ src/main/java/cn/yfbai/shopbackend/security/SecurityConfiguration.java
@@ -0,0 +1,20 @@
+package cn.yfbai.shopbackend.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .anyRequest().authenticated()
+            .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+}
